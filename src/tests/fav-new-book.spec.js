1b7a154fab035e3289cfdab1b3978b521af110e9
dikonius <eazy777er@gmail.com>
2025-06-03 11:21:56 +0200

    minor final changes in code

--- src/tests/fav-new-book.spec.js
+++ src/tests/fav-new-book.spec.js
@@ -7,13 +7,6 @@ test.describe('Testing favourite book feature', () => {
     
     test('Clicking heart button for "testTitle" adds marked new book to favourites', async ({ page }) => {
         
-        // 1. Click on "Lägg till bok" navigation button and label "Titel" must be visible
-        // 2. Fill form: "testTitle" in Titel field and "testAuthor" in författare field
-        // 3. Click on "Lägg till ny bok" knappen under Författare field. 
-        // 4. Click on "Katalog" navigation button so text "testAuthor" will be visible.
-        // 5. Click on "Heart" button at "testTitle" book so heart icon will remain visible.
-        // 6. Click on "Mina böcker" navigation button so text "testTitle" will be visible.
-        
         const addBookButton = page.getByRole('button', {name: 'Lägg till bok'})
         const catalogueButton = page.getByRole('button', {name: 'Katalog'})
         const visibleTestAuthor = page.getByText('testAuthor')
@@ -40,10 +33,10 @@ test.describe('Testing favourite book feature', () => {
         await catalogueButton.click({ timeout: 500 })
         await expect(visibleTestAuthor).toBeVisible({ timeout: 500 })
         
-        // 1. Click on "Heart" button at "testTitel" book so heart icon will remain visible.
+        // 5. Click on "Heart" button at "testTitle" book so heart icon will remain visible.
         await heartButton.click({ timeout: 500 });
         
-        // 2. Click on "Mina böcker" navigation button so text "testTitel" will be visible.
+        // 6. Click on "Mina böcker" navigation button so text "testTitle" will be visible.
         await myBooksButton.click({ timeout: 500 });
         await expect(visibleMyFavs).toBeVisible({ timeout: 500 });
         
